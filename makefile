57a04b7da50f22b7ff9422f521d498dc8b350b32
h4r0015k <nikhilsahani@pm.me>
2018-10-15 20:28:08 +0530

    Added basic ls

--- makefile
+++ makefile
@@ -3,3 +3,6 @@ SRC = ./src/
 
 cat: $(SRC)cat.c
 	$(CC) -o cat $(SRC)cat.c
+
+ls: $(SRC)ls.c
+	$(CC) -o ls $(SRC)ls.c
---
+++ src/ls.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+int compare(const void *a, const void *b)
+{
+    return strcmp(((char*)a),((char*)b));
+}
+
+void print_dir(char *dir) 
+{
+    DIR *d = opendir(dir);
+
+    if(d == NULL) {
+	perror(dir);
+	return;
+    }
+
+    struct dirent *p;
+
+    while((p = readdir(d)) != NULL) {
+	printf("%s\t",p->d_name);
+    }
+    printf("\n");
+
+}
+
+void process_args(int argc, char **argv)
+{
+    int tmp = 1;
+    while(tmp < argc) {
+	print_dir(argv[tmp]);
+	++tmp;
+    }
+}
+
+int main(int argc, char *argv[]) 
+{
+    char buf[50];
+    
+    if(argc > 1) {
+	process_args(argc, argv);
+	exit(EXIT_SUCCESS);
+    }
+
+    getcwd(buf, 50);
+    print_dir(buf);
+
+    exit(EXIT_SUCCESS);
+}
